cb670b00ec232e02df16465fa9d1400e80a056b4
Conor Cussell <contact@conorcussell.co.uk>
2017-01-30 07:45:15 +0000

    Adding stuff

--- img.js
+++ img.js
@@ -1,6 +1,6 @@
 var gm = require('gm');
 
-gm('img/sofar/events.jpg')
+gm('img/sofar/spotify-integration.jpg')
     .resize(15, 15)
     .toBuffer('GIF', function (error, buffer) {
         console.log('data:image/gif;base64,' + buffer.toString('base64'));
--- js/main.js
+++ js/main.js
@@ -1,6 +1,17 @@
 function ready() {
-  var images = [].slice.call(document.querySelectorAll('.js-lazy-load'));
-  images.map(function(img) { img.src = img.getAttribute('data-src') });
+  var lastKnownScrollPosition = 0;
+  var ticking = false;
+
+  window.addEventListener('scroll', function(e) {
+    lastKnownScrollPosition = window.scrollY;
+    if (!ticking) {
+      window.requestAnimationFrame(function() {
+        getImages(lastKnownScrollPosition);
+        ticking = false;
+      });
+    }
+    ticking = true;
+  });
 }
 
 if (document.readyState !== 'loading') {
@@ -9,6 +20,17 @@ if (document.readyState !== 'loading') {
   document.addEventListener('readystatechange', function() {
     if (document.readyState === 'interactive') {
       ready();
+    }
+  });
+}
+
+
+
+function getImages(scrollPos) {
+  var images = [].slice.call(document.querySelectorAll('.js-lazy-load'));
+  images.map(function(image) {
+    if((image.getBoundingClientRect().top - window.innerHeight) < 50) {
+      image.src = image.getAttribute('data-src');
     }
   });
 }
