9ba9f8e973838c2bea311d5d2ea70db2755f597f
Conor Cussell <contact@conorcussell.co.uk>
2017-03-03 17:40:12 +0000

    REmove unused tuff

--- tools/img.js
+++
@@ -1,7 +0,0 @@
-var gm = require('gm');
-
-gm('img/island/petite1.jpg')
-    .resize(15, 15)
-    .toBuffer('GIF', function (error, buffer) {
-        console.log('data:image/gif;base64,' + buffer.toString('base64'));
-    });
